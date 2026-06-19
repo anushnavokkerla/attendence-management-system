d3b9f9ae9d053645c2f0689164f693475e4af856
anushnavokkerla <anushnavokkerla06@gmail.com>
2026-06-19 14:25:59 +0530

    add a file

---
+++ frontend/javascript-practice.js
@@ -0,0 +1,46 @@
+console.log("welcome students")
+// var a=10
+// var b="10"
+// console.log(a===b)
+// var name ="Anushna"
+// var name ="Arun"
+name ="pranthi"
+console.log(name)
+var name="kumar"
+console.log(name)
+name ="aruna"
+console.log(name)
+
+// function greet () {
+//     var a = "welcome"
+//     console.log(a)
+// }
+// console.log(c)
+// console.log(a)
+
+// if(true){
+//     let c = 100
+//     console.log(c)
+// }
+
+
+// let a = 10
+// a = 10
+
+// const a = 10
+// a = 10
+
+//question: take 3 numbers and print which one is greater among them
+let a = 10;
+let b = 25;
+let c = 15;
+
+if (a > b && a > c) {
+    console.log(a + " is the greatest number");
+}
+else if (b > a && b > c) {
+    console.log(b + " is the greatest number");
+}
+else {
+    console.log(c + " is the greatest number");
+}
